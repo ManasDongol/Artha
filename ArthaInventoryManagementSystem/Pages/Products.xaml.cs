7122db1e2fbbde1397c40d2cdf938202fa09acbf
Manas Dongol <manasdongol27@gmail.com>
2026-05-24 21:16:43 +0545

    ci: add GitHub Actions workflow for WPF build

---
+++ ArthaInventoryManagementSystem/Pages/Products.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ArthaInventoryManagementSystem.Pages;
+
+public partial class Products : Page
+{
+    public Products()
+    {
+        InitializeComponent();
+    }
+}
